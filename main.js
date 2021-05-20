7ec841b9b780ae38b016e22479d7d7ac7fcc78c9
Christian Ndu <christiannduh@gmail.com>
2021-05-20 11:04:57 +0100

    celsius converter done

---
+++ main.js
@@ -0,0 +1,16 @@
+var convertFahrToCelsius = (num) => {
+	let celsius;
+	if (Array.isArray(num)) {
+		return `[${num}] is not a valid number but a/an array`;
+	}
+	if (typeof num !== "number") {
+		return `${num} is not a valid number but a/an ${typeof num}`;
+	}
+	if (typeof parseInt(num) === "number") {
+		celsius = (num - 32) * (5 / 9);
+		return celsius.toFixed(4);
+	}
+};
+
+const result = convertFahrToCelsius(0);
+console.log(result);
